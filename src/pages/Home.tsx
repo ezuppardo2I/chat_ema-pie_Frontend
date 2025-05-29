32be2ba0858cdcf3e148e84e1f620a697bd0a723
ezuppardo2I <ezuppardo@2innovation.it>
2025-05-29 15:12:03 +0200

    signin

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,37 @@ import {
   CognitoUserPool,
   CognitoUserAttribute,
   type ISignUpResult,
+  CognitoUser,
 } from "amazon-cognito-identity-js";
 
 import { poolData } from "../config";
+import { useState } from "react";
 
 export default function Home() {
+  const [isLogged, setIsLogged] = useState(false);
+
+  async function putUser(user: CognitoUser, email: string, username: string) {
+    console.log(user);
+
+    const res = await fetch(
+      "https://athx0w7rcf.execute-api.eu-west-2.amazonaws.com/dev/user",
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          userID: user.getUsername,
+          username: username,
+          email: email,
+        }),
+      }
+    );
+  }
+
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const email = formData.get("email")!.toString();
+    const username = formData.get("username")!.toString();
     const password = formData.get("password")!.toString();
     const userPool = new CognitoUserPool(poolData);
     const attributeList = [];
@@ -26,12 +48,12 @@ export default function Home() {
       attributeList,
       [],
       function (err: Error | undefined, result: ISignUpResult | undefined) {
-        if (err) {
-          alert(err.message || JSON.stringify(err));
+        if ((err as any).code === "UsernameExistException") {
+          alert(err?.message || JSON.stringify(err));
           return;
+        } else {
+          putUser(result!.user, email, username);
         }
-        var cognitoUser = result?.user;
-        console.log("user name is " + cognitoUser?.getUsername());
       }
     );
   }
@@ -43,9 +65,12 @@ export default function Home() {
   return (
     <>
       <div className="containerLogin">
-        <input type="email" name="email" id="email" required />
-        <input type="password" name="password" id="password" required />
-        <button type="submit">Login</button>
+        <form onSubmit={handleSubmit}>
+          <input type="username" name="username" id="username" required />
+          <input type="email" name="email" id="email" required />
+          <input type="password" name="password" id="password" required />
+          <button type="submit">Registrati</button>
+        </form>
         <a onClick={handleSignIn}>Registrati</a>
       </div>
     </>
