627666b930f78f03dba02ded63c556ecc3917c51
pstalteri2I <pstalteri@2innovation.it>
2025-05-29 15:13:51 +0200

    fix errori

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          userID: user.getUsername,
+          userID: user.getUsername(),
           username: username,
           email: email,
         }),
@@ -48,7 +48,7 @@ export default function Home() {
       attributeList,
       [],
       function (err: Error | undefined, result: ISignUpResult | undefined) {
-        if ((err as any).code === "UsernameExistException") {
+        if ((err as any).code === "UsernameExistsException") {
           alert(err?.message || JSON.stringify(err));
           return;
         } else {
@@ -66,7 +66,7 @@ export default function Home() {
     <>
       <div className="containerLogin">
         <form onSubmit={handleSubmit}>
-          <input type="username" name="username" id="username" required />
+          <input type="text" name="username" id="username" required />
           <input type="email" name="email" id="email" required />
           <input type="password" name="password" id="password" required />
           <button type="submit">Registrati</button>
