df02e42dadf5f48df14ce79cf79824be2dbb5d29
ezuppardo2I <ezuppardo@2innovation.it>
2025-06-04 15:34:28 +0200

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -118,6 +118,7 @@ export default function Home() {
   }
 
   async function handleIotConnection(lobby: any) {
+    setMessagesList([]);
     const info = await fetchAuthSession();
     connectToIoT(info.identityId);
     setActiveLobby(lobby);
